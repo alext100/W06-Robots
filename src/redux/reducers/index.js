844698c88b8cef946d21ce928ea132b8b1b9864b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-08 22:00:17 +0100

    usersReducer to rootReducer was added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import robotsReducer from "./robotsReducer";
+import usersReducer from "./usersReducer";
 
 const rootReducer = combineReducers({
   robots: robotsReducer,
+  user: usersReducer,
 });
 
 export default rootReducer;
